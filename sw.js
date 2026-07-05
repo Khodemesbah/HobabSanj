092df9306ceb2e763c32b490087ec6382f7807a8
Khodemesbah <m.mesbahabbaspour@gmail.com>
2026-07-05 22:53:25 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* با هر انتشار، نام کش را همراه ?v= عوض کن تا نسخهٔ جدید جایگزین شود */
-const CACHE = "hobab-v0.9.6";
-const ASSETS = ["./", "./index.html", "./styles.css?v=0.9.6", "./app.js?v=0.9.6", "./manifest.json", "./icon.svg",
+const CACHE = "hobab-v0.9.7.1";
+const ASSETS = ["./", "./index.html", "./styles.css?v=0.9.7.1", "./app.js?v=0.9.7.1", "./manifest.json", "./icon.svg",
   "./fonts/AbarMid-Regular.woff2", "./fonts/AbarMid-SemiBold.woff2",
   "./fonts/AbarMid-Bold.woff2", "./fonts/AbarMid-ExtraBold.woff2"];
 
